89886471ac642a4130dd7fd899b1cae61beb73f9
Jesus alberto Rojas gamboa <jesusrojas@MacBook-Pro-de-Jesus.local>
2021-10-01 19:16:46 +0200

    iterationCasi

--- iteration3.js
+++ iteration3.js
@@ -1,1 +1,17 @@
 console.log(10*5)
+
+console.log(10/2)
+
+console.log(15/8)
+
+// y= 10
+// z= 5
+// x= y + z
+// console.log(x)
+// ESTA OK
+
+y= 10
+z= 5
+x= y * z
+console.log(x)
+
---
+++ iteration4.js
@@ -0,0 +1,25 @@
+// let avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+// console.log(avengers[0])
+// // // esta ok
+
+// let avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"]
+// avengers[0] = "iron man";
+// console.log(avengers)
+// //  esta ok
+
+// let avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"]
+// [0],[1],[2]
+
+// let rickAndMortyCharacters = ["Rick", "Beth", "Jerry"]
+// rickAndMortyCharacters.push("Morty","Summer")
+
+// console.log(rickAndMortyCharacters[4])
+
+// let rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+// rickAndMortyCharacters.pop()
+// console.log(rickAndMortyCharacters[0],rickAndMortyCharacters[4])
+
+// let rickAndMortyCharacters = ["Rick", "Beth", "Jerry", "Morty", "Summer", "Lapiz Lopez"];
+// rickAndMortyCharacters.splice(1,1)
+
+// console.log(rickAndMortyCharacters)
