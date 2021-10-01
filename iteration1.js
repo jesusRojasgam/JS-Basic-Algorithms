eeea335dd602d1baf98f8d5f09509b30a893b769
Jesus alberto Rojas gamboa <jesusrojas@MacBook-Pro-de-Jesus.local>
2021-10-01 17:27:15 +0200

    iteracion1

---
+++ iteration1.js
@@ -0,0 +1,12 @@
+let myFavoriteHero = "hulk"
+let x = 50
+let h = 5
+let y = 10
+let z = h + y
+
+console.log(z)
+
+const character = {name: 'Jack Sparrow', age: 10};
+
+age = 25
+console.log(age)
---
+++ iteration2.js
@@ -0,0 +1,22 @@
+const character = {name: 'Jack Sparrow', age: 10};
+
+age = 25
+console.log(age)
+
+firstName = 'Jon'; 
+lastName = 'Snow'; 
+age = 24;
+
+console.log("soy " + firstName + " " + lastName + " tengo " + age + " años y me gustan los lobos")
+
+    
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+ 
+let totalPrice = toy1.price + toy2.price
+
+console.log(totalPrice)
+
+let globalBasePrice = 10000;
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
